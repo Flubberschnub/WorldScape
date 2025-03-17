5737fdaff9fb508cda02af565f341ad5a78d1e04
VivekMadarapu <vvkmdrp@gmail.com>
2025-03-17 15:27:28 -0500

    Added offsets to noise generation

--- Assets/Scripting/Terrain Generation/FractalPerlinNoiseHeightMapper.cs
+++ Assets/Scripting/Terrain Generation/FractalPerlinNoiseHeightMapper.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Scripting.Terrain_Generation
 {
     using UnityEngine;
@@ -6,50 +8,49 @@ namespace Scripting.Terrain_Generation
         menuName = "Scriptable Objects/FractalPerlinNoiseHeightMapper")]
     public class FractalPerlinNoiseHeightMapper : HeightMapper
     {
-
         public float xScale = 1f;
         public float zScale = 1f;
         public float amplitude = 1f;
+
+        // Offsets to avoid symmetrical patterns
+        public float offsetX;
+        public float offsetZ;
 
         public float persistence = 0.5f;
         public float lacunarity = 2f;
         public int octaves = 4;
 
+        private void OnEnable()
+        {
+            offsetX = Random.Range(0f, 9999999f);
+            offsetZ = Random.Range(0f, 9999999f);
+        }
+
         public override Vector3[] ApplyHeightMap(Vector3 positionOffset, Vector3[] vertices, int xSize, int zSize)
         {
-            Vector3[] newVertices = new Vector3[vertices.Length];
-
+            var newVertices = new Vector3[vertices.Length];
             for (int i = 0; i < vertices.Length; i++)
             {
                 float x = vertices[i].x + positionOffset.x;
                 float z = vertices[i].z + positionOffset.z;
-
-                float y = FractalPerlinNoise(x * xScale, z * zScale) * amplitude;
-
+                float y = FractalPerlinNoise((x + offsetX) * xScale, (z + offsetZ) * zScale) * amplitude;
                 newVertices[i] = new Vector3(vertices[i].x, y, vertices[i].z);
             }
-
             return newVertices;
         }
 
         private float FractalPerlinNoise(float x, float z)
         {
-
-            float total = 0;
-            float frequency = 1;
-            float curAmplitude = 1;
-
+            float total = 0f;
+            float frequency = 1f;
+            float curAmplitude = 1f;
             for (int i = 0; i < octaves; i++)
             {
                 total += Mathf.PerlinNoise(x * frequency, z * frequency) * curAmplitude;
-
                 curAmplitude *= persistence;
                 frequency *= lacunarity;
             }
-
             return total;
-
         }
-
     }
 }
