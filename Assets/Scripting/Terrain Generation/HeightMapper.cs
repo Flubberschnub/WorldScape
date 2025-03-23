b8f1e50240c946d9d05b6248ede4aeb7258c5e1d
VivekMadarapu <vvkmdrp@gmail.com>
2025-03-22 21:11:43 -0500

    Removed unnecessary files

--- Assets/Scripting/Terrain Generation/HeightMapper.cs
+++
@@ -1,9 +0,0 @@
-namespace Scripting.Terrain_Generation
-{
-    using UnityEngine;
-
-    public abstract class HeightMapper : ScriptableObject
-    {
-        public abstract Vector3[] ApplyHeightMap(Vector3 positionOffset, Vector3[] vertices, int xSize, int zSize);
-    }
-}
